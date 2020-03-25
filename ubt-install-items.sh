077a1de2edaafd13b73ddc8513ac3aaf46fe6c84
colbjenn83 <37814938+colbjenn83@users.noreply.github.com>
2020-03-25 16:16:25 -0500

    Update ubt-install-items.sh

--- ubt-install-items.sh
+++ ubt-install-items.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 #Stage 1 - DevOps Tools
 cd ~/Downloads
+#AWS CLI
+echo Downloading and Installing AWS CLI...
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 #Sublime Text
-echo Downloading Sublime Text...
+echo Downloading and Installing Sublime Text...
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add 
 sudo apt install -y apt-transport-https software-properties-common
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 #VS Code
-echo Downloading VS Code...
+echo Downloading and Installing VS Code...
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 #PowerShell
-echo Downloading PowerShell...
+echo Downloading and Installing PowerShell...
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt install -y gnome-tweak-tool nmap wireshark curl git python3.7 filezilla python-pip default-jre default-jdk powershell sublime-text code
 
 #Stage 2 - Misc. Tools
-echo Downloading Spotify...
+echo Downloading and Installing Spotify...
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo add-apt-repository ppa:papirus/papirus
@@ -35,7 +40,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
 sudo apt update
 
 #Stage 4 - Hacking Tools
-sudo apt install -y wifite netdiscover hydra hashcat binwalk ettercap john metasploit-framework
+sudo apt install -y wifite netdiscover hydra hashcat binwalk ettercap john beef-xss metasploit-framework
 sudo service postgresql start
 sudo msfdb init
 
