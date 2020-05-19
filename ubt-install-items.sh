d626b3fea87382da07dc87e58d720c2e17702551
colbjenn83 <37814938+colbjenn83@users.noreply.github.com>
2020-05-19 16:34:09 -0500

    Update ubt-install-items.sh

--- ubt-install-items.sh
+++ ubt-install-items.sh
@@ -7,6 +7,7 @@ sleep 10
 echo Downloading and Installing Sublime Text...
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
 sudo apt install -y sublime-text
 sleep 10
 #AWS CLI
@@ -19,6 +20,7 @@ sleep 10
 echo Downloading and Installing VS Code...
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
 sudo apt install -y code
 sleep 10
 #PowerShell
@@ -27,6 +29,7 @@ sleep 10
 #sudo dpkg -i packages-microsoft-prod.deb
 #sudo apt update
 #sudo add-apt-repository universe
+#sudo apt install -y powershell
 #sleep 10
 #Stage 2 - DevOps Tools
 echo Downloading and Installing DevOps Utilities...
