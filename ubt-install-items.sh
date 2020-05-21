798968d3338d7f44b7ada72ae965d672fcddbf54
colbjenn83 <37814938+colbjenn83@users.noreply.github.com>
2020-05-21 08:23:20 -0500

    Update ubt-install-items.sh

--- ubt-install-items.sh
+++ ubt-install-items.sh
@@ -28,32 +28,37 @@ echo Downloading and Installing MS Repos...
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
+sleep 3
 sudo add-apt-repository universe
-sleep 10
+sleep 3
 #Stage 2 - DevOps Tools
 echo Downloading and Installing DevOps Utilities...
 sudo apt update
+sleep 3
 sudo apt install -y dotnet-sdk-3.1 netdiscover nmap wireshark putty virtualbox python3-pip default-jre default-jdk golang ruby-full ansible
-sleep 10
+sleep 3
 #Stage 3 - Misc. Tools
 echo Downloading and Installing Spotify...
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo add-apt-repository ppa:papirus/papirus
+sleep 3
 sudo apt update
+sleep 3
 sudo apt install -y spotify-client neofetch gimp audacity vlc gnome-tweak-tool papirus-icon-theme gnome-shell-extensions
-sleep 10
+sleep 3
 #Stage 4 - Install Hacking Tools and Dependencies
 echo Install Hacking Tools and Dependencies...
 sudo apt install -y libpcap-dev build-essential openssl libreadline8 libreadline-dev zlib1g-dev nodejs libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion aircrack-ng wifite crunch postgresql postgresql-contrib john hashcat wireless-tools ettercap-graphical hostapd isc-dhcp-server iptables lighttpd dsniff reaver xterm 
 #Stage 5 - Other Tasks
-cd ~
+cd ~/
 sudo mkdir Tools Scripts Loot Wordlists Wallpaper Avatars Ansible
 cd Ansible
 sudo mkdir Playbooks Inventory
-sleep 10
+sleep 3
 #Stage 6 - git Downloads "Tools"
 cd ~/Tools
+sleep 3
 echo Downloading Tools from GitHub...
 sudo git clone https://github.com/hak5darren/USB-Rubber-Ducky.git
 #Stage 6 - Install Themes
@@ -64,9 +69,9 @@ sudo cp -r * /usr/share/themes
 #Stage 7 - Download Assets
 echo Downloading Assets...
 cd ~/Downloads/InstallScripts/
-sudo cp Corvo* ~/Avatars
-sudo cp NeoTokyo* ~/Wallpaper
-cd ~
+sudo cp Corvo*.* ~/Avatars
+sudo cp NeoTokyo*.* ~/Wallpaper
+cd ~/
 #Stage 8 - Clean up
 echo Finished Quick Install Script...
 
